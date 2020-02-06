ae17a799680905a979dbef5d0862fa5327f02ffe
sevheng <sevhengluy@gmail.com>
2020-02-06 11:19:56 +0700

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,4 @@ echo "Copy nginx/conf.d"
 cp -r /nginx/conf.d /etc/nginx
 
 echo "Start nginx"
-nginx -g "daemon off;"
+while :; do sleep 6h & wait $${!}; nginx -s reload; done & nginx -g "daemon off;"
