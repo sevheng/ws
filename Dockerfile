04bf250a0e0df8bdcd3fbeca3ef444f8f1a73894
sevheng <sevhengluy@gmail.com>
2020-02-24 14:24:39 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
 FROM nginx:1.16-alpine
 
+# Install certbot
+RUN apk add --no-cache certbot
+
+# Copy renew cron script
+COPY ./renew /etc/periodic/daily/renew
+RUN chmod +x /etc/periodic/daily/renew
+
+RUN mkdir /var/lib/certbot
+
 COPY ./docker-entrypoint.sh .
 
 RUN chmod +x ./docker-entrypoint.sh
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+
+email=$EMAIL
+domain=$DOMAIN
+
+echo "email : ${email}"
+echo "doamin : ${domain}"
 
 echo "Copy nginx.conf"
 cp /nginx/nginx.conf /etc/nginx/nginx.conf
@@ -5,5 +12,12 @@ cp /nginx/nginx.conf /etc/nginx/nginx.conf
 echo "Copy nginx/conf.d"
 cp -r /nginx/conf.d /etc/nginx
 
+# Get certs
+certbot certonly -n -d $domain,www.$domain \
+  --standalone --preferred-challenges http --email $email --agree-tos --expand
+
+# Kick off cron
+/usr/sbin/crond -f -d 8 &
+
 echo "Start nginx"
-while :; do sleep 6h & wait $${!}; nginx -s reload; done & nginx -g "daemon off;"
+while :; do sleep 6h & wait $!; nginx -s reload; done & nginx -g "daemon off;"
