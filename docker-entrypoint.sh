7b412c52c3919e9acf66eb9d4f6f398eed4dbbbd
sevheng <sevhengluy@gmail.com>
2020-02-24 14:36:18 +0700

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,4 +20,4 @@ certbot certonly -n -d $domain,www.$domain \
 /usr/sbin/crond -f -d 8 &
 
 echo "Start nginx"
-while :; do sleep 6h & wait $!; nginx -s reload; done & nginx -g "daemon off;"
+nginx -g "daemon off;"
