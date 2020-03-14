2063da9e05264a8fab84988705af9d83b040827e
sevheng <sevhengluy@gmail.com>
2020-03-15 01:20:21 +0700

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,7 +13,7 @@ echo "Copy nginx/conf.d"
 cp -r /nginx/conf.d /etc/nginx
 
 # Get certs
-certbot certonly -n -d $domain \
+certbot certonly -n $domain \
   --standalone --preferred-challenges http --email $email --agree-tos --expand
 
 # Kick off cron
